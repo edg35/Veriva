8aed70f257690879a81162213202efecf55442dd
ethan green <ethandavid111@gmail.com>
2026-03-02 17:20:51 -0500

    setup placeholder functions to be implemented on backend

--- backend/routes/waitlist.py
+++ backend/routes/waitlist.py
@@ -1,18 +1,28 @@
+import models
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel, EmailStr
 from sqlalchemy.exc import IntegrityError
 
-import models
 from database import AsyncSessionLocal
 
-router = APIRouter()
+router = APIRouter(prefix='/api/waitlist', tags=['waitlist'])
 
 
 class WaitlistRequest(BaseModel):
     email: EmailStr
 
 
-@router.post("/waitlist")
+@router.get("/")
+async def get_waitlist():
+    return {"message": "To be implemented"}
+
+
+@router.delete("/remove/{email}", status_code=204)
+async def remove_from_waitlist(email: EmailStr):
+    return {"message": "To be implemented"}
+
+
+@router.post("/join", status_code=201)
 async def join_waitlist(body: WaitlistRequest):
     async with AsyncSessionLocal() as session:
         waitlist_entry = models.WaitList(email=body.email)
@@ -20,5 +30,11 @@ async def join_waitlist(body: WaitlistRequest):
         try:
             await session.commit()
         except IntegrityError:
-            raise HTTPException(status_code=409, detail="Email already on waitlist")
+            raise HTTPException(
+                status_code=409, detail="Email already on waitlist")
     return {"message": "Email added to waitlist"}
+
+
+@router.put("/update/{email}", status_code=200)
+async def update_waitlist_email(email: EmailStr, body: WaitlistRequest):
+    return {"message": "To be implemented"}
